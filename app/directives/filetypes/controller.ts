66ace450f952be751f79be81922d853127b5160d
sroth <stuartaroth@github.com>
2015-12-11 01:07:59 -0700

    Added some stats

--- app/directives/filetypes/controller.ts
+++ app/directives/filetypes/controller.ts
@@ -4,30 +4,13 @@ export = FiletypesController;
 
 class FiletypesController {
     static $inject = [
-        '$scope',
         'htManager'
     ];
 
     constructor(
-        $scope,
         public htManager
     ) {
-        var unwatch = $scope.$watch('ctrl.htManager.har', (newVal) => {
-            if(newVal) {
-                this.getFiletypeStats();
-                unwatch();
-            }
-        });
+
     }
 
-    public stats = null;
-
-    public getFiletypeStats() {
-        var array = [];
-        var requests = this.htManager.har.log.entries;
-        for(var i = 0; i < requests.length; i++) {
-            array.push(requests[i].request.url);
-        }
-        this.stats = array;
-    }
 }
--- app/services/manager.ts
+++ app/services/manager.ts
@@ -6,6 +6,7 @@ export = Manager;
 
 class Manager {
     private har:iHar = null;
+    public statistics:any = null;
 
     public getHar() {
         return this.har;
@@ -13,6 +14,9 @@ class Manager {
 
     public setHar(fileContent:string) {
         this.har = this.parseHar(fileContent);
+        if(this.har) {
+            this.calculateStatistics();
+        }
     }
 
     private parseHar(fileContent:string):iHar {
@@ -21,6 +25,36 @@ class Manager {
         }
         catch(exception) {
             return null;
+        }
+    }
+
+    private calculateStatistics() {
+        var responseStatistics = {};
+        this.addContentType(responseStatistics);
+        this.statistics = responseStatistics;
+    }
+
+    private addContentType(responseStatistics:any) {
+        var contentTypeMap = {};
+        var entries = this.har.log.entries;
+        for(var i = 0; i < entries.length; i++) {
+            var headers = entries[i].response.headers;
+            for(var j = 0; j < headers.length; j++) {
+                if(headers[j].name.toLowerCase() == "content-type") {
+                    this.addToMap(contentTypeMap, headers[j].value);
+                }
+            }
+        }
+        responseStatistics.contentTypeMap = contentTypeMap;
+    }
+
+    private addToMap(map:any, item:string) {
+        if(map[item]) {
+            map[item] += 1;
+        }
+        else {
+            console.log("wait here");
+            map[item] = 1;
         }
     }
 }
